e3b2ecc7ec55b6ef70c290fbad1067333bd9db06
jesus zatarain <jesus.517.zatarain@gmail.com>
2017-02-17 01:46:05 -0700

    Se creo la ventanta con los 9 botones para el gato y al presionarlos se pone un circulo y una x alternamente

---
+++ src/Ventana_TicTacToe/Casilla.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ventana_TicTacToe;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author pequesillo
+ */
+public class Casilla {
+    JButton A;
+    int B;
+    
+    Casilla(){
+        A=new JButton();
+        B=0;
+    }
+}
